3a637448c6180964a9f3328e71e3ac870d51b316
MYasvanth <yasvanthkohli@gmail.com>
2026-09-04 21:26:42 +0530

    Add initial model definitions and exports in __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.orm import MLModel, ModelVersion, Deployment, DeploymentEvent, ModelMetric, LifecycleStage, DeploymentStatus, LIFECYCLE_TRANSITIONS
+
+__all__ = ["MLModel", "ModelVersion", "Deployment", "DeploymentEvent", "ModelMetric", "LifecycleStage", "DeploymentStatus", "LIFECYCLE_TRANSITIONS"]
