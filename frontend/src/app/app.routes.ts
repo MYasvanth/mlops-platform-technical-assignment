d1002d9a920a15a37c815210c9f875cba87791c9
MYasvanth <yasvanthkohli@gmail.com>
2026-09-05 00:19:38 +0530

    Adds route for comparing model versions
    Enables navigation to a version comparison view for models,
    improving accessibility of model version comparison features
    within the application.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ModelsComponent } from './models/models.component';
 import { ModelDetailComponent } from './model-detail/model-detail.component';
+import { VersionCompareComponent } from './model-detail/version-compare.component';
 import { DeploymentsComponent } from './deployments/deployments.component';
 import { MonitoringComponent } from './monitoring/monitoring.component';
 
@@ -8,6 +9,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'models', pathMatch: 'full' },
   { path: 'models', component: ModelsComponent },
   { path: 'models/:id', component: ModelDetailComponent },
+  { path: 'models/:id/compare', component: VersionCompareComponent },
   { path: 'deployments', component: DeploymentsComponent },
   { path: 'monitoring', component: MonitoringComponent },
 ];
